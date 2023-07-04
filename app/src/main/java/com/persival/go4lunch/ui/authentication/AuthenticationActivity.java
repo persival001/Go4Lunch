29f211a07ad7c70318d228fb2c76faaae3a12222
persival001 <persival@free.fr>
2023-07-04 09:56:36 +0200

    authentication error handled

--- app/src/main/java/com/persival/go4lunch/ui/authentication/AuthenticationActivity.java
+++ app/src/main/java/com/persival/go4lunch/ui/authentication/AuthenticationActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -22,6 +23,7 @@ import com.persival.go4lunch.ui.main.MainActivity;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -29,11 +31,6 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class AuthenticationActivity extends AppCompatActivity {
 
     private AuthenticationViewModel viewModel;
-
-    private final ActivityResultLauncher<Intent> signInActivityResultLauncher = registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
-        result -> handleResponseAfterSignIn(result.getResultCode(), result.getData())
-    );
 
     public static Intent navigate(Context context) {
         return new Intent(context, AuthenticationActivity.class);
@@ -48,30 +45,17 @@ public class AuthenticationActivity extends AppCompatActivity {
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             startMainActivity();
         } else {
-            // Authentication providers
-            List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build(),
-                new AuthUI.IdpConfig.EmailBuilder().build());
-
-            // Launch the authentication activity
-            Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setTheme(R.style.LoginTheme)
-                .setAvailableProviders(providers)
-                .setLogo(R.drawable.ic_logo_auth)
-                .setIsSmartLockEnabled(false, true)
-                .build();
-
-            signInActivityResultLauncher.launch(signInIntent);
+            createSignInIntent();
         }
     }
 
     private void startMainActivity() {
         Intent startMainActivity = new Intent(this, MainActivity.class);
         this.startActivity(startMainActivity);
-    }
+    }    private final ActivityResultLauncher<Intent> signInActivityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        result -> handleResponseAfterSignIn(result.getResultCode(), result.getData())
+    );
 
     // Method that handles response after SignIn Activity close
     private void handleResponseAfterSignIn(int resultCode, @Nullable Intent data) {
@@ -88,6 +72,7 @@ public class AuthenticationActivity extends AppCompatActivity {
     private void handleSignInError(@Nullable IdpResponse response) {
         if (response == null) {
             Log.e(TAG, "Authentication canceled");
+            createSignInIntent();
             return;
         }
 
@@ -96,10 +81,41 @@ public class AuthenticationActivity extends AppCompatActivity {
 
             if (errorCode == ErrorCodes.NO_NETWORK) {
                 Log.e(TAG, "No network available");
+                Toast.makeText(this, R.string.no_network, Toast.LENGTH_SHORT).show();
+                createSignInIntent();
             } else if (errorCode == ErrorCodes.UNKNOWN_ERROR) {
                 Log.e(TAG, "Unknown error occurred");
+                Toast.makeText(this, R.string.unknown_error, Toast.LENGTH_SHORT).show();
+                createSignInIntent();
+            } else if (Objects.requireNonNull(response.getError()
+                    .getLocalizedMessage())
+                .contains("auth/invalid-credential")
+            ) {
+                Log.e(TAG, "Invalid credentials error");
+                Toast.makeText(this, R.string.invalid_credentials, Toast.LENGTH_SHORT).show();
+                createSignInIntent();
             }
         }
+    }
+
+    private void createSignInIntent() {
+        // Authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.GoogleBuilder().build(),
+            new AuthUI.IdpConfig.FacebookBuilder().build(),
+            new AuthUI.IdpConfig.TwitterBuilder().build(),
+            new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the authentication activity
+        Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setTheme(R.style.LoginTheme)
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.ic_logo_auth)
+            .setIsSmartLockEnabled(false, true)
+            .build();
+
+        signInActivityResultLauncher.launch(signInIntent);
     }
 
     @Override
@@ -107,4 +123,8 @@ public class AuthenticationActivity extends AppCompatActivity {
         super.onDestroy();
         signInActivityResultLauncher.unregister();
     }
+
+
+
+
 }
